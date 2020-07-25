6ec7c1b3dc2a8f658ab59e441137173b0fae8813
ramraaj25 <ramraaj.bishnoie@gmail.com>
2020-07-25 18:01:06 +0530

    improved

--- tree.py
+++ tree.py
@@ -6,9 +6,6 @@ WIDTH = 750
 HEIGHT = 600
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 COLOR = (255, 255, 255)
-INIT_LENGTH = 200
-RBRANCH_ANGLE = math.pi / 4
-LBRANCH_ANGLE = math.pi - math.pi / 4
 
 
 def main():
@@ -17,23 +14,25 @@ def main():
     pygame.display.set_caption("Fractal Tree")
     run = True
     while run:
+        draw_fractal_tree(WIN, WIDTH // 2, HEIGHT, 90, 10)
+        pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-            draw_fractal_tree(WIN, WIDTH // 2, HEIGHT, -90, 10)
+            draw_fractal_tree(WIN, WIDTH // 2, HEIGHT, 90, 10)
             pygame.display.flip()
 
     pygame.quit()
 
 
-def draw_fractal_tree(win, a, b, pos, deepness):
+def draw_fractal_tree(win, a, b, ang, deepness):
 
     if deepness:
-        c = a + int(math.cos(math.radians(pos)) * deepness * 10.0)
-        d = b + int(math.sin(math.radians(pos)) * deepness * 10.0)
+        c = a + int(math.cos(math.radians(ang)) * deepness * 10.0)
+        d = b - int(math.sin(math.radians(ang)) * deepness * 10.0)
         pygame.draw.line(win, COLOR, (a, b), (c, d), 1)
-        draw_fractal_tree(win, c, d, pos - 25, deepness - 1)
-        draw_fractal_tree(win, c, d, pos + 25, deepness - 1)
+        draw_fractal_tree(win, c, d, ang - 25, deepness - 1)
+        draw_fractal_tree(win, c, d, ang + 25, deepness - 1)
 
 
 if __name__ == "__main__":
